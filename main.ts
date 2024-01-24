44f090b7365c0fdafe85c0a5e26a937550ab4aa1
789-ux269 <157656500+789-ux269@users.noreply.github.com>
2024-01-24 18:33:47 -0500

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,72 @@
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    projectile = sprites.createProjectileFromSprite(img`
+        . . . . . . . . 
+        . . . . . . . . 
+        . . . . . . . . 
+        . . . . . . . . 
+        . . . 7 7 . . . 
+        . . . 7 7 . . . 
+        . . . 7 7 . . . 
+        . . . 7 7 . . . 
+        `, ship, 0, -140)
+    projectile.startEffect(effects.coolRadial, 100)
+    music.play(music.melodyPlayable(music.pewPew), music.PlaybackMode.InBackground)
+})
+controller.combos.attachCombo("llr", function () {
+    projectile2 = sprites.createProjectileFromSprite(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . b . . . . . . . 
+        . . . . . . . b d b . . . . . . 
+        . . . . . . . c d c . . . . . . 
+        . . . . . . . c 5 c . . . . . . 
+        . . . . . . c d 5 d c . . . . . 
+        . . . b c c d 5 5 5 d c c b . . 
+        . . b d d 5 5 5 5 5 5 5 d d b . 
+        . . . b c c d 5 5 5 d c c b . . 
+        . . . . . . c d 5 d c . . . . . 
+        . . . . . . . c 5 c . . . . . . 
+        . . . . . . . c d c . . . . . . 
+        . . . . . . . b d b . . . . . . 
+        . . . . . . . . b . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, ship, 0, -140)
+})
+info.onLifeZero(function () {
+    game.gameOver(false)
+})
+sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Enemy, function (sprite, otherSprite) {
+    sprite.destroy()
+    otherSprite.destroy(effects.disintegrate)
+    info.changeScoreBy(1)
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
+    scene.cameraShake(4, 500)
+    otherSprite.destroy(effects.disintegrate)
+    sprite.startEffect(effects.fire, 200)
+    info.changeLifeBy(-1)
+    music.play(music.melodyPlayable(music.bigCrash), music.PlaybackMode.InBackground)
+})
+let projectile2: Sprite = null
+let projectile: Sprite = null
+let ship: Sprite = null
+music.play(music.stringPlayable("C G F D A G B A ", 120), music.PlaybackMode.InBackground)
+let asteroids = [
+sprites.space.spaceSmallAsteroid1,
+sprites.space.spaceSmallAsteroid0,
+sprites.space.spaceAsteroid0,
+sprites.space.spaceAsteroid1,
+sprites.space.spaceAsteroid4,
+sprites.space.spaceAsteroid3
+]
+ship = sprites.create(sprites.space.spaceRedShip, SpriteKind.Player)
+ship.setStayInScreen(true)
+ship.bottom = 130
+controller.moveSprite(ship, 100, 100)
+info.setLife(3)
+effects.starField.startScreenEffect()
+game.onUpdateInterval(500, function () {
+    projectile = sprites.createProjectileFromSide(asteroids[randint(0, asteroids.length - 1)], 0, 75)
+    projectile.setKind(SpriteKind.Enemy)
+    projectile.x = randint(10, 150)
+})
